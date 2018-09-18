5bb86beeac34f14b23f12d0109a6e2cfb874a6f6
adamszabolcs <szabolcs.adam91@gmail.com>
2018-09-18 17:06:28 +0200

    default setup first phase created, cards now on tableau

--- src/com/codecool/klondike/Game.java
+++ src/com/codecool/klondike/Game.java
@@ -198,7 +198,15 @@ public class Game extends Pane {
 
     public void dealCards() {
         Iterator<Card> deckIterator = deck.iterator();
-        //TODO
+        for (int i = 0; i < tableauPiles.size(); i++) {
+            for (int j = 0; j <= i; j++) {
+                Card card = deckIterator.next();
+                tableauPiles.get(i).addCard(card);
+                addMouseEventHandlers(card);
+                getChildren().add(card);
+            }
+            tableauPiles.get(i).getTopCard().flip();
+        }
         deckIterator.forEachRemaining(card -> {
             stockPile.addCard(card);
             addMouseEventHandlers(card);
